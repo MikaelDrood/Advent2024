65d48612c51705c007a1f611a8f6843b2ef45c4d
m.barinov <m.barinov@cian.ru>
2024-12-18 11:27:03 +0300

    Day16 part2

---
+++ Sources/Day17.swift
@@ -0,0 +1,14 @@
+import Algorithms
+
+struct Day17: AdventDay {
+    // Save your data in a corresponding text file in the `Data` directory.
+    var data: String
+
+    func part1() -> Any {
+        return 0
+    }
+
+    func part2() -> Any {
+        return 0
+    }
+}

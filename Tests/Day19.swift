f0a054a8188e0dfdd069ee36f40e1da302ba3dbc
m.barinov <m.barinov@cian.ru>
2024-12-19 11:47:45 +0300

    Dat19 part1

---
+++ Tests/Day19.swift
@@ -0,0 +1,32 @@
+import XCTest
+
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+final class Day19Tests: XCTestCase {
+  // Smoke test data provided in the challenge question
+  let testData = Day19.testData()
+//  let testData = """
+//r, wr, b, g, bwu, rb, gb, br
+//
+//brwrr
+//bggr
+//gbbr
+//rrbgbr
+//ubwu
+//bwurrg
+//brgr
+//bbrgwb
+//"""
+
+  func testPart1() throws {
+    let challenge = Day19(data: testData)
+    XCTAssertEqual(String(describing: challenge.part1()), "6")
+  }
+
+  func testPart2() throws {
+    let challenge = Day19(data: testData)
+    XCTAssertEqual(String(describing: challenge.part2()), "6,1")
+  }
+}

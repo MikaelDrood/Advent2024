eb6d9a4e0b025d2259c7ecb764c0e9e6e78a714f
Mikael Barinov <mikhailbarinov.dev@gmail.com>
2024-12-23 22:04:45 +0300

    Day21 init

---
+++ Tests/Day21.swift
@@ -0,0 +1,21 @@
+import XCTest
+
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+final class Day21Tests: XCTestCase {
+  // Smoke test data provided in the challenge question
+  let testData = """
+"""
+
+  func testPart1() throws {
+    let challenge = Day21(data: testData)
+    XCTAssertEqual(String(describing: challenge.part1()), "37327623")
+  }
+
+  func testPart2() throws {
+    let challenge = Day21(data: testData)
+    XCTAssertEqual(String(describing: challenge.part2()), "23")
+  }
+}

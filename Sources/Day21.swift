fb8b1993169b88df2fd374bb1fec74455768b413
Mikael Barinov <mikhailbarinov.dev@gmail.com>
2024-12-24 14:36:57 +0300

    Day21 part1.2

--- Sources/Day21.swift
+++ Sources/Day21.swift
@@ -4,14 +4,35 @@ struct Day21: AdventDay {
     // Save your data in a corresponding text file in the `Data` directory.
     var data: String
 
-    let numeric = [["7", "8", "9"],
-                   ["4", "5", "6"],
-                   ["1", "2", "3"],
-                   ["", "0", "A"]]
-    let directional = [["", "^", "A"],
-                       ["<", "v", ">"]]
+    static let numeric = [["7", "8", "9"],
+                          ["4", "5", "6"],
+                          ["1", "2", "3"],
+                          ["", "0", "A"]]
+    static let directional = [["", "^", "A"],
+                              ["<", "v", ">"]]
 
-    enum Dir: String, CaseIterable {
+    static let coords: [Dir: (Int, Int)] = {
+        var coords: [Dir: (Int, Int)] = [:]
+
+        for r in 0 ..< directional.count {
+            for c in 0 ..< directional[0].count {
+                guard let ch = directional[r][c].first,
+                      let dir = Dir(rawValue: ch)
+                else { continue }
+
+                coords[dir] = (r, c)
+            }
+        }
+
+        return coords
+    }()
+
+    static func distance(_ dir: Character) -> Int {
+        let coords = coords[Dir(rawValue: dir)!]!
+        return abs(0 - coords.0) + abs(2 - coords.1)
+    }
+
+    enum Dir: Character, CaseIterable {
         case up = "^"
         case right = ">"
         case down = "v"
@@ -23,6 +44,15 @@ struct Day21: AdventDay {
             case .right: (0, 1)
             case .down: (1, 0)
             case .left: (0, -1)
+            }
+        }
+
+        var priority: Int {
+            switch self {
+            case .right: 3
+            case .up: 2
+            case .down: 1
+            case .left: 0
             }
         }
     }
@@ -49,16 +79,17 @@ struct Day21: AdventDay {
 
         for code in codes {
             let seqLen = enter(code)
-            complexity += (Int(String(code.dropLast())) ?? 0) * seqLen
+            print(seqLen, Int(String(code.dropLast()))!)
+            complexity += Int(String(code.dropLast()))! * seqLen
         }
 
         return complexity
     }
 
     func enter(_ code: String) -> Int {
-        let numPath = searchPath(numeric, code, (3, 2))
-        let roboPath = searchPath(directional, numPath, (0, 2))
-        let historiansPath = searchPath(directional, roboPath, (0, 2))
+        let numPath = searchPath(Self.numeric, code, (3, 2))
+        let roboPath = searchPath(Self.directional, numPath, (0, 2))
+        let historiansPath = searchPath(Self.directional, roboPath, (0, 2))
 
         print(numPath, numPath.count)
         print(roboPath, roboPath.count)
@@ -73,7 +104,7 @@ struct Day21: AdventDay {
 
         for sym in word {
             var heap = Heap<Node>()
-            heap.insert(Node(r: r, c: c, len: 0, seq: path))
+            heap.insert(Node(r: r, c: c, len: 0, seq: ""))
 
             var visited: [Node: Int] = [:]
 
@@ -83,14 +114,17 @@ struct Day21: AdventDay {
                 guard isValid(n, map) else { continue }
 
                 let memoLen = visited[n, default: Int.max]
-                guard n.len <= memoLen else { continue }
+                guard n.len < memoLen else { continue }
 
                 visited[n] = n.len
 
                 guard map[n.r][n.c] != String(sym) else {
                     r = n.r
                     c = n.c
-                    path = n.seq + "A"
+                    path += n.seq.sorted(by: {
+                        //Self.distance($0) < Self.distance($1)
+                        Dir(rawValue: $0)!.priority > Dir(rawValue: $1)!.priority
+                    }) + "A"
                     break
                 }
 
@@ -98,7 +132,7 @@ struct Day21: AdventDay {
                     let next = Node(r: n.r + dir.mut.0,
                                     c: n.c + dir.mut.1,
                                     len: n.len + 1,
-                                    seq: n.seq + dir.rawValue)
+                                    seq: n.seq + String(dir.rawValue))
                     heap.insert(next)
                 }
             }
